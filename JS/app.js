1da4b29d90b5bafcb62f12ba1c99c9f46c251530
Alessiasansalone <145045809+Alessiasansalone@users.noreply.github.com>
2024-07-06 20:52:29 +0200

    ADD: numeri da 1 a 100

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+// console.log('ciao')
+
+/* Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+ per i multipli di 3 stampi “Fizz” al posto del numero
+ per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
+
+//creiamo un ciclo che conti i numeri d 1 a 100
+for (let i = 0; i < 100; i++) {
+    // creiamo una variabile che indichi a JS di partire dall'1
+    let num = i + 1;
+
+    // stampiamo il risultato
+    console.log (num)
+}
