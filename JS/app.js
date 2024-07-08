c06cbf8d7b64cdbada00b596f38b419bf6b055d3
Alessiasansalone <145045809+Alessiasansalone@users.noreply.github.com>
2024-07-08 12:36:08 +0200

    ADD: end

--- JS/app.js
+++ JS/app.js
@@ -5,31 +5,50 @@
  per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
 
+// prendiamo ul dal DOM
+const ulElement = document.getElementById('lista')
+
 //creiamo un ciclo che conti i numeri d 1 a 100
 for (i = 0; i < 100; i++) {
 
     // creiamo una variabile che indichi a JS di partire dall'1
     let num = i + 1 // number
-    // stampo num
-    console.log(num) // number
 
-    // let resto = num % 3
-    let resto3 = num % 3 // number
-    // let resto = num % 5
-    let resto5 = num % 5 // number
-    // let resto = num % 15
-    let resto15 = num % 15 // number
+    // STAMPA IN CONSOLE
 
     // se i % 3 => 'Fizz'
-    if (resto15 == 0) {
-        console.log(num + 'FizzBuzz') // string
+    if (num % 15 == 0) {
+        console.log('FizzBuzz') // string
+    }
+    // altrimenti se i % 5 => 'Buzz'
+    else if (num % 5 == 0) {
+        console.log('Buzz') // string
+    }
+    // altrimenti se i % 15 => 'FizzBuzz'
+    else if (num % 3 == 0) {
+        console.log('Fizz') // string
+    }
+    // altrimenti => num
+    else {
+        console.log(num)
+    }
+
+    // STAMPA IN HTML 
+
+    // se i % 3 => 'Fizz'
+    if (num % 15 == 0) {
+        ulElement.innerHTML = ulElement.innerHTML + '<li> FizzBuzz </li>'
     }
     // se i % 5 => 'Buzz'
-    else if (resto5 == 0) {
-        console.log(num + 'Buzz') // string
+    else if (num % 5 == 0) {
+        ulElement.innerHTML = ulElement.innerHTML + '<li> Buzz </li>'
     }
     // se i % 15 => 'FizzBuzz'
-    else if (resto3 == 0) {
-        console.log(num + 'Fizz') // string
+    else if (num % 3 == 0) {
+        ulElement.innerHTML = ulElement.innerHTML + '<li> Fizz </li>'
+    }
+    else {
+        ulElement.innerHTML = ulElement.innerHTML + '<li>' + num + '</li>'
+
     }
 }
